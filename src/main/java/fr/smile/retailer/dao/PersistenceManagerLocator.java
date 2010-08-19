d372e5ab25691824045fd94c8e8f9f20b8f02a0d
ansam <ansam@vostok.(none)>
2010-08-19 12:59:28 +0300

    Delete unnecessary objects

--- src/main/java/fr/smile/retailer/dao/PersistenceManagerLocator.java
+++
@@ -1,8 +0,0 @@
-package fr.smile.retailer.dao;
-
-import javax.jdo.PersistenceManager;
-
-public interface PersistenceManagerLocator {
-	
-	public PersistenceManager getPersistenceManager();
-}
--- src/test/java/fr/smile/retailer/dao/LocalDailySalesDAO.java
+++
@@ -1,7 +0,0 @@
-package fr.smile.retailer.dao;
-
-import fr.smile.retailer.model.DailySales;
-
-public class LocalDailySalesDAO extends AbstractTestHelperAppEngineDAO<DailySales> implements IDailySalesDAO {
-
-}
